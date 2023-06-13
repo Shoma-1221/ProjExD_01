57dbddf313aeb542a0dc28388e81aa4e8fc732b3
SHOMA <c0b22070e1@edu.teu.ac.jp>
2023-06-13 16:53:37 +0900

    演習課題

--- pygame_image.py
+++ pygame_image.py
@@ -7,20 +7,25 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 3, 1.0),pg.transform.rotozoom(kk_img, 6, 1.0),pg.transform.rotozoom(kk_img, 9, 1.0),pg.transform.rotozoom(kk_img, 6, 1.0),pg.transform.rotozoom(kk_img, 3, 1.0)]
     tmr = 0
+    tmr2 = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x1 = tmr%1600
+        x1 = tmr2%3200
         screen.blit(bg_img, [-x1, 0])
-        screen.blit(bg_img, [1520-x1, 0])
-        screen.blit(kk_imgs[tmr%2],[300,200])
+        screen.blit(bg_img2, [1600-x1, 0])
+        screen.blit(bg_img, [3200-x1,0])
+        screen.blit(bg_img2, [4800-x1, 0])
+        screen.blit(kk_imgs[tmr%6],[300,200])
         pg.display.update()
-        tmr += 1      
-        clock.tick(100)
+        tmr += 1     
+        tmr2 += 10 
+        clock.tick(10)
 
 
 if __name__ == "__main__":
